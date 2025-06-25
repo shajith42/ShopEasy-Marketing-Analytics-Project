a64a56150f99bfbedb2fcf8c25837a9a7f657fa7
Shajith <121110865+shajith42@users.noreply.github.com>
2025-06-25 19:31:28 +0530

    Add files via upload

---
+++ Sql/Products.sql
@@ -0,0 +1,13 @@
+SELECT * FROM dbo.products;
+SELECT 
+    ProductID, 
+    ProductName, 
+    Price,
+    Category,
+
+    CASE 
+        WHEN Price < 50 THEN 'Low'
+        WHEN Price BETWEEN 50 AND 200 THEN 'Medium'
+        ELSE 'High'
+    END AS PriceCategory
+FROM dbo.products;
